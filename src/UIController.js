3772f621c054d7dce9aa154dfc5be66741cce54e
Andrew Perotti <Perotti28@gmail.com>
2025-03-31 13:02:55 -0700

    Move variables into event listener where needed
    Beginners error I had a variable pull the input fields entered value at load and not when the button is clicked, which resulted in moving other variables into that event listener function as well.
    I also changed that event listener function to be async so we could await the fetch response.

--- src/UIController.js
+++ src/UIController.js
@@ -1,23 +1,24 @@
-import { getWeather } from "./weatherOperations";
+import { getWeather } from "./weatherOperations.js";
 
-const uiController = function () {
-  let getWeatherButton = document.querySelector("#get-weather-button");
-  let userCityInput = document.querySelector("#city-input-filed").value;
+let getWeatherButton = document.querySelector("#get-weather-button");
+
+//event listener on 'Search' button in the html that triggers getWeather and then uses the build functions to fill in the information
+getWeatherButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  let userCityInput = document.querySelector("#city-input-field").value;
+  console.log(userCityInput);
   let convertedUserCity = userCityInput.split(" ").join("-");
   let returnedData;
 
-  //event listener on 'Search' button in the html that triggers getWeather and then uses the build functions to fill in the information
-  getWeatherButton.addEventListener("click", async (e) => {
-    e.preventDefault();
-    returnedData = await getWeather(convertedUserCity);
-    buildToday(returnedData);
-  });
-  //function that builds content for current/todays weather
-  function buildToday(weatherData) {
-    //weatherData will be the converted data
-  }
+  returnedData = await getWeather(convertedUserCity);
+  buildToday(returnedData);
+});
+//function that builds content for current/todays weather
+function buildToday(weatherData) {
+  //weatherData will be the converted data
+  console.log(weatherData);
+}
 
-  //error reporting functionality for when users try to enter invalid data to the input field
+//error reporting functionality for when users try to enter invalid data to the input field
 
-  //button to change Fahrenheit to Celsius
-};
+//button to change Fahrenheit to Celsius
