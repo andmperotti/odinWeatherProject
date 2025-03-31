2337cd116fbd6fdd5537819975f974e0f2375375
Andrew Perotti <Perotti28@gmail.com>
2025-03-31 12:46:35 -0700

    Add variable to hold eventual returned data
    This variable will be given data when the Search button is clicked and results in a resolved promise object.
    The buildToday function will take in that data and pluck from it the current days weather data to then fill the #current-weather section elements children elements with data.
    Converted this file from just holding variables and a function to holding a module pattern, which I don't think I've correctly exported yet.

--- src/UIController.js
+++ src/UIController.js
@@ -1,8 +1,23 @@
-//function that builds content for current/todays weather
-function buildToday(todaysWeather) {}
+import { getWeather } from "./weatherOperations";
 
-//event listener on 'Search' button in the html that triggers getWeather and then uses the build functions to fill in the information
+const uiController = function () {
+  let getWeatherButton = document.querySelector("#get-weather-button");
+  let userCityInput = document.querySelector("#city-input-filed").value;
+  let convertedUserCity = userCityInput.split(" ").join("-");
+  let returnedData;
 
-//error reporting functionality for when users try to enter invalid data to the input field
+  //event listener on 'Search' button in the html that triggers getWeather and then uses the build functions to fill in the information
+  getWeatherButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    returnedData = await getWeather(convertedUserCity);
+    buildToday(returnedData);
+  });
+  //function that builds content for current/todays weather
+  function buildToday(weatherData) {
+    //weatherData will be the converted data
+  }
 
-//button to change Fahrenheit to Celsius
+  //error reporting functionality for when users try to enter invalid data to the input field
+
+  //button to change Fahrenheit to Celsius
+};
