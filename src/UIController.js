7b93531bb72577a27fdfe8a91f955a6121c71715
Andrew Perotti <Perotti28@gmail.com>
2025-03-31 11:35:37 -0700

    Move unnecessary pseudo code from weatherOps
    This file will be where the creation of data into pre made html elements happens, which didn't need to be in weatherOperations.

---
+++ src/UIController.js
@@ -0,0 +1,8 @@
+//function that builds content for current/todays weather
+function buildToday(todaysWeather) {}
+
+//event listener on 'Search' button in the html that triggers getWeather and then uses the build functions to fill in the information
+
+//error reporting functionality for when users try to enter invalid data to the input field
+
+//button to change Fahrenheit to Celsius
